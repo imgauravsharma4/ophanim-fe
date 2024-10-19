f8310f76b2797a58314bb875c5c2e41f880f21e7
deepoa <deepeaaa45@gmail.com>
2024-10-19 12:46:10 +0530

    news page

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const NewsPage = () => {
   return (
     <div>
-      News Page
+      <div className="container">
+        <p className="text-center fs-1 ">News & Articles</p>
+        <p className="text-center">Stay Updated with tech and implementation through our news section</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewsPage
+export default NewsPage;
--- src/pages/pricing/index.js
+++ src/pages/pricing/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const PricingPage = () => {
-  return <div>Pricing Page</div>;
+  return (
+    <div className="container">
+      <div className="row">
+<div className="col-6"><p>{`Homepage ${'>'} Pricing`}</p></div>
+
+      </div>
+    </div>
+  );
 };
 
 export default PricingPage;
