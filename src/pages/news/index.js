8edb8049ee57387af8cb4fe6283bef697528f889
deepoa <deepeaaa45@gmail.com>
2024-10-20 17:40:18 +0530

    updaed code

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Articles } from "../../utlis/variables";
-import Post from "../../assests/images/Big Post.svg";
+import Post from "../../assests/images/mainImage.svg";
+import NewsCard from "../../Components/NewsCard/NewsCard";
 
 const NewsPage = () => {
   return (
@@ -11,58 +11,38 @@ const NewsPage = () => {
           Stay Updated with tech and implementation through our news section
         </p>
 
-        <div className="text-center mb-5">
+        <div className="text-center mb-5 position-relative">
           <img src={Post} alt="seo ranking" className="img-fluid" />
         </div>
-
-        <div className="row">
-          {Articles.map((article, index) => (
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 mb-4" key={index}>
-              <div style={{ width: "400px", height: "359.2px" }}>
-                <img
-                  className="img-fluid"
-                  src={article.image}
-                  alt={`Card image cap ${index}`}
-                />
-              </div>
-              <div>
-                <h5>{article.title}</h5>
-                <p>{article.description}</p>
-                <p>View Post</p>
-                <p
-                  style={{
-                    width: "108px",
-                    background: "#121416",
-                    height: "1px",
-                    cursor: "pointer",
-                  }}
-                ></p>
-              </div>
-            </div>
-          ))}
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <p className="sub-headig-image position-absolute">Travel</p>
         </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "20px",
-          }}
-        >
-          <button
-            style={{
-              width: "240px",
-              height: "54px",
-              borderRadius: "7px",
-              borderColor: "#8C8C8C",
-              backgroundColor: "#fff",
-              color: "#000",
-              fontSize: "16px",
-              cursor: "pointer",
-              marginBottom:"50px"
-            }}
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <p
+            className="position-absolute sub-heading-2"
+            style={{ margin: "0", lineHeight: "1.1" }}
           >
-            Load More
-          </button>
+            Richird Norton photorealistic rendering as
+            <span style={{ display: "block", margin: 0, padding: 0 , textAlign:'center'}}>real photos</span>  
+            </p>
+        </div>
+        <div className="position-absolute sub-heading-3">
+        <div style={{ display: "flex", justifyContent: "center" ,marginLeft:"300px"}}>
+
+          <p>Progressively incentivize cooperative systems through technically sound 
+            <br/>
+            functionalities. The credibly productivate seamless data.</p>
+          </div>
+          <div className="position-absolute" style={{display:"flex", justifyContent:"center", alignItems:"center", alignContent:"center", marginLeft:"550px"}} >
+            <button className="read-now ">Read Now </button>
+          </div>
+     
+        </div>
+
+        <NewsCard />
+
+        <div className="button-container">
+          <button className="button-load-more">Load More</button>
         </div>
       </div>
     </div>
