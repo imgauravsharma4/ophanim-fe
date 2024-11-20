43b56bb5013d5e97e8d223ccb06651e0775da71b
deepoa <deepeaaa45@gmail.com>
2024-11-20 14:45:05 +0530

    code updated

--- src/Components/PortfolioSlider/PortfolioSlider.js
+++ src/Components/PortfolioSlider/PortfolioSlider.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import sliderImg from '../../assests/images/slideimg.png'
+import slider2 from '../../assests/images/slider2.png'
+import slider3 from '../../assests/images/slider3.png'
 
 const responsive = {
   desktop: {
@@ -22,20 +25,18 @@ const responsive = {
 
 const sliderImageUrl = [
   {
-    url: "https://i2.wp.com/www.geeksaresexy.net/wp-content/uploads/2020/04/movie1.jpg?resize=600%2C892&ssl=1",
+    url: slider2,
   },
   {
-    url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-kids-movies-2020-call-of-the-wild-1579042974.jpg?crop=0.9760858955588091xw:1xh;center,top&resize=480:*",
+    url: slider2,
   },
   {
-    url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*",
+    url: slider3,
   },
   {
-    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQS82ET2bq9oTNwPOL8gqyoLoLfeqJJJWJmKQ&usqp=CAU",
+    url: slider3,
   },
-  {
-    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTdvuww0JDC7nFRxiFL6yFiAxRJgM-1tvJTxA&usqp=CAU",
-  },
+ 
 ];
 
 const PortfolioSlider = () => {
