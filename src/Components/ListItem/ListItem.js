f44dc69a536b005a4e5cae0914dbcbe69d655d1e
Gaurav <contactme.gauravsharma@gmail.com>
2024-12-06 00:25:11 +0530

    updated listing and fix font size

--- src/Components/ListItem/ListItem.js
+++ src/Components/ListItem/ListItem.js
@@ -5,7 +5,7 @@ const ListItem = ({ item, index, icon }) => {
   return (
     <li key={`item${index}`}>
       <img src={icon ? icon : CheckCricleImage} alt='check circle' className='img-fluid' />
-      {item}
+      {item.trim()}
     </li>
   );
 };
